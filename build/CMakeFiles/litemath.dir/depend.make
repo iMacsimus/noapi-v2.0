fffb0c9a9e6c59dd848eb288ecf31791c34f2945
Maksim Kunts <kuncz.maks@mail.ru>
2023-10-26 00:13:13 +0300

    Remove trash files (build, .vscode, .bmp)

--- build/CMakeFiles/litemath.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for litemath.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/noapi.dir/src/dummy.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/noapi.dir/src/dummy.cpp.o: \
- /home/imacsimus/MyProjects/noapi-v2.0/src/dummy.cpp \
- /usr/include/stdc-predef.h
